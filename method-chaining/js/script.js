d7c26cda2b07012efcfc09337b1b0d36a5550360
OrlaBr <orlabreslin@yahoo.com>
2020-01-10 12:32:19 +0000

    Edits

--- method-chaining/js/script.js
+++ method-chaining/js/script.js
@@ -2,11 +2,19 @@ $(document).ready(function() {
  //
  // add your jQuery code here
 $("#button1").mouseenter(function(){
-$('#button1').removeClass("makeRed").addClass("makeBorder");
+    $('#button1').removeClass("makeRed").addClass("makeBorder");
 });
 
 $("#button1").mouseleave(function(){
-$("#button1").removeClass("makeBorder").addClass("makeRed");
+    $("#button1").removeClass("makeBorder").addClass("makeRed");
 });
+
+$("button").click(function(){
+    $("p").slideToggle(2000);
+});
+
+$("button").click(function(){
+    $("#button2").fadeIn(slow).fadeOut(slow);
+})
 
 }); 
